82a7c07c7cf77c9d46d335b17ed20b7551d67658
Edwin <ewarandia@gmail.com>
2020-02-20 21:56:39 -0400

    added functionality

--- 6AngularJS/js/controllers.js
+++ 6AngularJS/js/controllers.js
@@ -3,7 +3,18 @@
 var carAppControllers = angular.module('carAppControllers',[]);
 
 carAppControllers.controller('carListCtrl', ['$scope',
-    function($scope){
-        alert('Welcome to CarApp');
+    function($scope,Car){
+        $scope.cars = [{
+            "name": "New QASHQAI",
+            "snippet": "MADE WITH YOU IN MIND TO GIVE YOU THE HIGHEST QUALITY"
+        },
+        {
+            "name": "Veloster",
+            "snippet": "SPORTY LIKE A COUPE. ROOMY LIKE A SEDAN"
+        },
+        {
+            "name": "Navara",
+            "snippet": "THE NAVARA IS ALL ABOUT FREEDOM INCLUDING FREEDOM OF CHOISE"
+        }];
     }
 ]);
