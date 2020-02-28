a78103fee9442503f72fb32b50ccb7c56249c6b5
Edwin <ewarandia@gmail.com>
2020-02-27 23:47:12 -0400

    added more js file

--- 6AngularJS/js/controllers.js
+++ 6AngularJS/js/controllers.js
@@ -2,19 +2,8 @@
 
 var carAppControllers = angular.module('carAppControllers',[]);
 
-carAppControllers.controller('carListCtrl', ['$scope',
+carAppControllers.controller('carListCtrl', ['$scope', 'Car',
     function($scope,Car){
-        $scope.cars = [{
-            "name": "New QASHQAI",
-            "snippet": "MADE WITH YOU IN MIND TO GIVE YOU THE HIGHEST QUALITY"
-        },
-        {
-            "name": "Veloster",
-            "snippet": "SPORTY LIKE A COUPE. ROOMY LIKE A SEDAN"
-        },
-        {
-            "name": "Navara",
-            "snippet": "THE NAVARA IS ALL ABOUT FREEDOM INCLUDING FREEDOM OF CHOISE"
-        }];
+        $scope.cars = Car.getCars();
     }
 ]);
--- 6AngularJS/js/services.js
+++ 6AngularJS/js/services.js
@@ -7,6 +7,20 @@ carAppServices.factory('Car', [
         return {
             notify: function(msg){
                 alert(msg);
+            },
+            getCars: function(){
+                var cars = [{
+                    "name":"NEW QASHQAI",
+                    "snippet": "MADE WITH TOU IN MIND TO GIVE YOU THE HIGHEST QUALITY"
+                },
+                {
+                    "name": "Veloster",
+                    "snippet":"SPORTY LIKE A COUPE. ROOMY LIKE A SEDAN"
+                },
+                {
+                    "name":"Navara",
+                    "snippet":"THE NAVARA IS ALL LABOUT FREEDOM INCLUDING FREEDOM OF CHOICE"
+                }];
             }
         }
     }
